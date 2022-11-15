b048e36e863f33f92789dfe16076697d90847408
ViktorAtanasof <atanasowviktor@gmail.com>
2022-11-15 16:04:13 +0200

    Create api service for loading themes and posts

---
+++ SoftUni Forum Workshop/app/src/app/api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+const apiURL = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  loadThemes() {
+    return this.httpClient.get(`${apiURL}/themes`);
+  }
+
+  loadPosts(limit?: number) {
+    return this.httpClient.get(`${apiURL}/posts${limit ? `?limit=${limit}` : ``}`);
+  }
+}
