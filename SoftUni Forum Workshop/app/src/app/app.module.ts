24b997c7ba24d8cd317b3319470eb1fb4b4a34a3
ViktorAtanasof <atanasowviktor@gmail.com>
2022-11-15 15:55:42 +0200

    Initial commit

---
+++ SoftUni Forum Workshop/app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ThemeListComponent,
+    RecentPostsComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
