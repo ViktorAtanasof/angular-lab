130e543100c25f035152d1b4df2fab3a63cffe1b
ViktorAtanasof <atanasowviktor@gmail.com>
2022-11-23 19:44:32 +0200

    Add routing

---
+++ SoftUni Forum Workshop/app/src/app/theme/theme-routing.module.ts
@@ -0,0 +1,30 @@
+import { RouterModule, Routes } from "@angular/router";
+import { MainComponent } from "./main/main.component";
+import { NewThemeComponent } from "./new-theme/new-theme.component";
+import { ThemeResolver } from "./resolvers/theme.resolver";
+import { ThemeDetailsComponent } from "./theme-details/theme-details.component";
+
+const routes: Routes = [
+    {
+        path: 'theme',
+        children: [
+            {
+                path: 'recent',
+                component: MainComponent
+            },
+            {
+                path: 'new',
+                component: NewThemeComponent
+            },
+            {
+                path: 'details/:id',
+                resolve: {
+                    theme: ThemeResolver
+                },
+                component: ThemeDetailsComponent
+            }
+        ]
+    }
+];
+
+export const ThemeRoutingModule = RouterModule.forChild(routes);
