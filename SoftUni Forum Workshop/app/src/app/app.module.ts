fccff19dbaa2204c6b435a3db727aff51f19af36
ViktorAtanasof <atanasowviktor@gmail.com>
2022-11-23 15:28:04 +0200

    Import httpclient module, shared module & main component

--- SoftUni Forum Workshop/app/src/app/app.module.ts
+++ SoftUni Forum Workshop/app/src/app/app.module.ts
@@ -7,18 +7,20 @@ import { ThemeListComponent } from './theme-list/theme-list.component';
 import { RecentPostsComponent } from './recent-posts/recent-posts.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './main/main.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ThemeListComponent,
     RecentPostsComponent,
-    MainComponent
+    MainComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
