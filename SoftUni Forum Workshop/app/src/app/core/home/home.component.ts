a1125bc76a15a7aea35f9b3fa41bcf62ca7f8d58
ViktorAtanasof <atanasowviktor@gmail.com>
2022-11-23 19:49:54 +0200

    Implement authentication logic

---
+++ SoftUni Forum Workshop/app/src/app/core/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  get isLoggedIn() {
+    return this.authService.isLoggedIn;
+  }
+
+  constructor(private authService: AuthService) { }
+
+}
