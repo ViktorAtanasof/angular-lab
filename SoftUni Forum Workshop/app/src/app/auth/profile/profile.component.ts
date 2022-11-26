be1a16a1dbedf5cd2d1f69411e1403b2daace25e
ViktorAtanasof <atanasowviktor@gmail.com>
2022-11-26 11:54:47 +0200

    Implement validation and profile handler

--- SoftUni Forum Workshop/app/src/app/auth/profile/profile.component.ts
+++ SoftUni Forum Workshop/app/src/app/auth/profile/profile.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { appEmailDomains } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
 
-  constructor() { }
+  showEditMode = false;
+  formSubmitted = false;
 
-  ngOnInit(): void {
+  get user() {
+    const { username, email, tel: telephone } = this.authService.user!;
+    const [ext, ...tel] = telephone.split(' ');
+    return {
+      username,
+      email,
+      tel: tel.join(' '),
+      ext
+    };
   }
 
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
+    ext: [''],
+    tel: [''],
+  })
+
+  constructor(private fb: FormBuilder, private authService: AuthService) { 
+    this.form.setValue(this.user);
+  }
+
+  toggleEditMode(): void {
+    this.showEditMode = !this.showEditMode;
+    if (this.showEditMode) {
+      this.formSubmitted = false;
+    }
+  }
+
+  saveProfile(): void {
+    this.formSubmitted = true;
+    if (this.form.invalid) { return; }
+    const { username, email, ext, tel } = this.form.value;
+    this.authService.user = {
+      username, email, tel: ext + ' ' + tel
+    } as any;
+    this.toggleEditMode();
+  }
 }
